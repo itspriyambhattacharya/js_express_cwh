24f6be988d9db187dae99bff2e1c65cac4b578fb
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-31 21:18:23 +0530

    feat: add error handling middleware for improved error responses

--- 03_basics_middlewares/app.js
+++ 03_basics_middlewares/app.js
@@ -4,8 +4,21 @@ const port = 3000;
 
 const idx = require("./routes/index");
 
+app.use(express.json()); // Build-in middleware
+app.use(express.urlencoded({ extended: true })); // Built-in middleware
 app.use(express.static("public"));
 app.use("/", idx);
+
+// Error handing middleware
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(404).send("Page not found");
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).send("Internal server error");
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
