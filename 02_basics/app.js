0646e6a3ff1266e8e2f15aac08f36a6adea6f77d
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-31 11:58:36 +0530

    feat: enhance app.js with GET and POST request logging

--- 02_basics/app.js
+++ 02_basics/app.js
@@ -2,8 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public")); // serving public files such as html, css, js or img
+
 app.get("/", (req, res) => {
+  console.log("It's a get request");
+
   res.send("Hello Priyam");
+});
+
+app.post("/", (req, res) => {
+  res.send("Hello post");
+  console.log("It's a post request");
 });
 
 app.listen(port, () => {
