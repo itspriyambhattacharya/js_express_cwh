03468c514c0e592507f8205dac073415d839f115
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-31 13:55:32 +0530

    refactor: reorganize route handling in app.js by consolidating index routes

--- 02_basics/app.js
+++ 02_basics/app.js
@@ -1,26 +1,12 @@
 const express = require("express");
-const blogs = require("./routes/blog");
 const app = express();
+const blogs = require("./routes/blog");
+const index = require("./routes/index");
 const port = 3000;
 
 app.use(express.static("public")); // serving public files such as html, css, js or img
 app.use("/blog", blogs); // Creating routes using Express.js Router
-
-app.get("/", (req, res) => {
-  console.log("It's a get request");
-
-  //   res.send("Hello Priyam");
-  res.sendFile("/public/templates/index.html", { root: __dirname });
-});
-
-app.get("/contact", (req, res) => {
-  res.redirect("/");
-});
-
-app.post("/contact", (req, res) => {
-  res.send("Hello post");
-  console.log("It's a post request");
-});
+app.use("/", index);
 
 app.listen(port, () => {
   console.log("App listening in port ", port);
