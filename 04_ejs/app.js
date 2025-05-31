a2f2881c9429212fcf5f9bfcc5ec4d8581f123d0
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-31 21:53:43 +0530

    feat: initialize Express application with routing and static file serving

---
+++ 04_ejs/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const idx = require("./routes/index");
+
+app.use(express.static("public"));
+app.use("/", idx);
+
+app.listen(port, () => {
+  console.log("App is listening");
+});
