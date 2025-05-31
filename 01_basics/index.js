4c76a11654850c4b484b9b32593b6790e2d33226
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-31 11:20:44 +0530

    feat: add about and blog routes with dynamic slug handling

--- 01_basics/index.js
+++ 01_basics/index.js
@@ -12,6 +12,28 @@ app.get("/contact", (req, res) => {
   res.send("Thanks for contacting us, we'll reach you shortly");
 });
 
+app.get("/about", (req, res) => {
+  res.send("About Us");
+});
+
+app.get("/blog", (req, res) => {
+  const data = res;
+  console.log(data);
+
+  res.send("Blog");
+});
+
+app.get("/blog/:slug", (req, res) => {
+  const p = req;
+  const data = req.params; // object
+  const val = data.slug;
+
+  // res.send(`Blog object: ${JSON.stringify(data)}`);
+  res.send(`Blog: ${val}`);
+  console.log(p);
+  console.log(req.query);
+});
+
 app.listen(port, () => {
   console.log(`Myapp listening on port ${port}`);
 });
