e619cd13c7f8bbe5f5b693da0837ff6cce54c14c
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-31 21:53:25 +0530

    feat: implement initial route setup with middleware for logging

---
+++ 04_ejs/routes/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const route = express.Router();
+
+route.use((req, res, next) => {
+  console.log("Route middleware");
+  next();
+});
+
+route.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+module.exports = route;
