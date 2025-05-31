e5c4fb3fd57273f43c8f5d90f128aeb4aa402c5d
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-31 13:55:21 +0530

    feat: implement index.js route handling with middleware and contact endpoints

---
+++ 02_basics/routes/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+
+// middleware that is specific to this router
+const timeLog = (req, res, next) => {
+  console.log("Time: ", Date.now());
+  next();
+};
+router.use(timeLog);
+
+router.get("/", (req, res) => {
+  console.log("It's a get request");
+
+  //   res.send("Hello Priyam");
+  res.sendFile(path.join(__dirname, "../public/templates/index.html"));
+  console.log(__dirname);
+});
+
+router.get("/contact", (req, res) => {
+  res.redirect("/");
+});
+
+router.post("/contact", (req, res) => {
+  res.send("Hello post");
+  console.log("It's a post request");
+});
+
+module.exports = router;
