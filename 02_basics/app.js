0e60588e19b8c74690d8a3605f370e1fa0e26a94
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-01 13:47:25 +0530

    refactor: reorganize middleware setup in app.js for clarity

--- 02_basics/app.js
+++ 02_basics/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
-const blogs = require("./routes/blog");
-const index = require("./routes/index");
 const port = 3000;
 
-app.use(express.static("public")); // serving public files such as html, css, js or img
+app.use(express.static("public")); // build-in middleware to fetch public files
+app.use(express.urlencoded({ extended: true })); // built-in middleware to fetch form data
+
+app.set("view engine", "ejs"); // set view engine
+
+const blogs = require("./routes/blog");
+const index = require("./routes/index");
+
 app.use("/blog", blogs); // Creating routes using Express.js Router
 app.use("/", index);
 
