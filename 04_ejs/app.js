3d289d24afac498874355d2accd5a2a5f443fb74
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-01 10:55:33 +0530

    feat: add route for about page in the Express application

--- 04_ejs/app.js
+++ 04_ejs/app.js
@@ -6,8 +6,10 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 const idx = require("./routes/index");
+const abt = require("./routes/about");
 
 app.use("/", idx);
+app.use("/about", abt);
 
 app.listen(port, () => {
   console.log("App is listening on port", port);
