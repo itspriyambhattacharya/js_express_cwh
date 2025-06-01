55843889e80eb3883b05e79e525aefe109473102
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-01 10:39:58 +0530

    fix: reorder code for clarity and improve console log message

--- 04_ejs/app.js
+++ 04_ejs/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const idx = require("./routes/index");
 
 app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+const idx = require("./routes/index");
+
 app.use("/", idx);
 
 app.listen(port, () => {
-  console.log("App is listening");
+  console.log("App is listening on port", port);
 });
