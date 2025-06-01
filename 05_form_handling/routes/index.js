b12ba7e09d430dc73bec0f748838f5223fbf892c
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-01 13:37:27 +0530

    feat: add POST route to handle form submissions and log input data

--- 05_form_handling/routes/index.js
+++ 05_form_handling/routes/index.js
@@ -11,4 +11,15 @@ route.get("/", (req, res) => {
   res.render("index");
 });
 
+route.post("/", (req, res) => {
+  console.log(req.body); // contains the data that came from form submission in object form
+  // object destructuring to store the values in separate variables
+  const { name, email, password } = req.body;
+  console.log(`Name is: ${name}`);
+  console.log(`Email is: ${email}`);
+  console.log(`Password is: ${password}`);
+
+  res.redirect("/");
+});
+
 module.exports = route;
