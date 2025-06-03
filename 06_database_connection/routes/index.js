f24d3ee9af56f2e5f9e49baf5cabacab3742b228
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-03 22:35:00 +0530

    feat: enhance middleware to query student data from MySQL and log results

--- 06_database_connection/routes/index.js
+++ 06_database_connection/routes/index.js
@@ -1,19 +1,45 @@
 const express = require("express");
 const route = express.Router();
+const mysql2 = require("mysql2");
+
+// Create a MySQL connection pool
+const pool = mysql2.createPool({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "mysql12345678",
+  database: "college2",
+});
 
 // middleware
 route.use((req, res, next) => {
   console.log("Index middleware");
+
+  pool.query("SELECT * FROM student", (err, rows, fields) => {
+    if (err) {
+      console.log("Database Query Error: ", err.message);
+    }
+    // console.log(rows);
+    rows.forEach((obj) => {
+      console.log(`Roll Number is: ${obj.rollNo}`);
+      console.log(`Name is: ${obj.name}`);
+      console.log(`Marks is: ${obj.marks}`);
+      console.log(`City is: ${obj.city}`);
+      console.log("\n");
+    });
+  });
+
   next();
 });
 
+// GET route
 route.get("/", (req, res) => {
   res.render("index");
 });
 
+// POST route
 route.post("/", (req, res) => {
-  // console.log(req.body);
-  const { name, email, password } = req.body; // here 'name', 'email', etc are the names given in the HTML input elements. If in the HTML input tag, the name given as 'myName' etc, the here, in req,body the keys would be myName, myEmail... etc
+  const { name, email, password } = req.body;
   console.log(`Name is ${name}`);
 
   res.redirect("/");
