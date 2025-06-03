ae68b200fde37885ff390a1d7cf542a3f28ec762
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-03 21:33:51 +0530

    feat: implement POST route for form submission in index.js

--- 06_database_connection/routes/index.js
+++ 06_database_connection/routes/index.js
@@ -11,4 +11,12 @@ route.get("/", (req, res) => {
   res.render("index");
 });
 
+route.post("/", (req, res) => {
+  // console.log(req.body);
+  const { name, email, password } = req.body; // here 'name', 'email', etc are the names given in the HTML input elements. If in the HTML input tag, the name given as 'myName' etc, the here, in req,body the keys would be myName, myEmail... etc
+  console.log(`Name is ${name}`);
+
+  res.redirect("/");
+});
+
 module.exports = route;
