a02f67c7c9cf43bceea845af85f1566e5936c1ff
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-04 10:51:34 +0530

    fix: add host variable for server configuration to run the app on mobile browser as well.

--- 07_form_handling/app.js
+++ 07_form_handling/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const host = "0.0.0.0";
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
