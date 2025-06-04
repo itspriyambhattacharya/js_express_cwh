ddc20f22d6ab519a84f40d95f3ebbf1d31ffef7c
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-04 10:42:29 +0530

    feat: add index route with middleware and hello world response

---
+++ 07_form_handling/routes/index.js
@@ -0,0 +1,15 @@
+const { log } = require("console");
+const express = require("express");
+const route = express.Router();
+
+// middleware
+route.use((req, res, next) => {
+  console.log("Index middleware");
+  next();
+});
+
+route.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+module.exports = route;
